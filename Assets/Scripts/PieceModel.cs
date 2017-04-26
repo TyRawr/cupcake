dbc16e4727e90f2bcedf512e440610734e8bdf1a
peccles <peccles@axis41.com>
2017-04-26 17:23:42 -0600

    PieceModel

---
+++ Assets/Scripts/PieceModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class PieceModel
+	{
+		private PieceColor color;
+		private PieceType type;
+
+		public PieceModel (PieceType type, PieceColor color)
+		{
+			this.type = type;
+			this.color = color;
+		}
+	}
+
+	public enum PieceColor 
+	{
+		PINK,
+		ORANGE,
+		GREEN,
+		PURPLE,
+		YELLOW
+	}
+
+	public enum PieceType 
+	{
+		NORMAL,
+		STRIPED,
+		DOTTED,
+		CANDLE,
+	}	
+
+	// GETTERS
+
+	public PieceColor GetColor () {
+		return this.color;
+	}
+	public PieceType GetType () {
+		return this.type;
+	}
+}
+
